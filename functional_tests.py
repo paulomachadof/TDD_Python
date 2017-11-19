da2a8e18043391d7c8452dbc27127349d8d36254
Paulo Machado <paulomachadof3@gmail.com>
2017-11-18 23:36:16 -0300

    Ate Cap3

--- functional_tests.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-browser = webdriver.Firefox(executable_path='/home/paulo/Downloads/geckodriver')
-browser.get('http://localhost:8000')
-
-assert 'Django' in browser.title
